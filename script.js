17ffb7ef04d19048135fc924098f47f865b3e403
Akshay sawant <134278084+Akshaysawant917@users.noreply.github.com>
2024-02-28 16:10:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const { getRandomValues } = require("crypto");
+
+const bg= document.querySelector(".bg");
+const btn=document.querySelector(".btn");
+
+btn.addEventListener("click",()=>{
+    bg.classList.add()
+})
